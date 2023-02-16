c2475a03c26447acdc40631b98993e696f2f7973
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-16 23:14:40 +0530

    Added Samrt Contract

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,39 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity >=0.7.0 <0.9.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+contract Upload {
+    struct Access {
+        address user;
+        bool access;
     }
+}
+mapping (address => string[]) value;
+mapping (address => mapping(address => bool)) ownership;
+mapping (address => Access[]) accessList;
+mapping (address => mapping(address => bool)) previousData;
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+function add(address _user, string memory url)  external  {
+    value[_user].push(url);
+}
+function allow(address user) external {
+    ownership[msg.sender][user] = true;
+    if(previousData[msg.sender][user]){
+        for (uint i=0; i<accessList[msg.sender].length; i++){
+            if(accessList[msg.sender][i].user == user){
+                accessList[msg.sender].push(Access(user, true));
+            }
+        }
+    }
+    else {
+        accessList[msg.sender].push(Access(user, true));
+        previousData[msg.sender][user]=true;
+    }
+}
+function disallow(address user) public {
+    ownership[msg.sender][user] = false;
+    for (uint i=0; i<accessList[msg.sender].length;i++){
+        if(accessList[msg.sender][i].user == user){
+            accessList[msg.sender][i].access=false
+        }
     }
 }
