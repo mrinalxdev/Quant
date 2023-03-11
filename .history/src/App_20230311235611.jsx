becdc1975bdc18bd7c283966ef3b28b1c1c59779
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-11 23:57:20 +0530

    Added TalwindCSS

--- .history/src/App_20230311235611.jsx
+++ .history/src/App_20230311235611.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Ho</h1>
+    
   )
 }
 
