8d1fda704b368d7e392caa70c5336ff715cc1cc2
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-21 17:03:17 +0530

    Added Navbar Section

---
+++ reddit/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Flex } from '@chakra-ui/react'
+
+
+const Navbar:React.FC = () => {
+  return (
+    <Flex bg='white'  height='44px' padding='6px 12px'>
+        <Flex>Logo</Flex>
+    </Flex>
+  )
+}
+
+export default Navbar
