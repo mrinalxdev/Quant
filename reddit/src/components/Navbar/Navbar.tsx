2449caaa4dc13f103e567fbc6d591ce232a6cb5b
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-21 23:07:43 +0530

    Added New Data

--- reddit/src/components/Navbar/Navbar.tsx
+++ reddit/src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { Flex } from '@chakra-ui/react'
 const Navbar:React.FC = () => {
   return (
     <Flex bg='white'  height='44px' padding='6px 12px'>
-        <Flex>Logo</Flex>
+        <Flex>Reddit Logo</Flex>
     </Flex>
   )
 }
