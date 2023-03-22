b72a4b508b481875f17acdf822ec5c98d0da94e6
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-22 22:26:59 +0530

    Added Classic Logo

--- reddit/src/components/Navbar/Navbar.tsx
+++ reddit/src/components/Navbar/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar:React.FC = () => {
   return (
     <Flex bg='white'  height='44px' padding='6px 12px'>
         <Flex align='center'>
-          <Image src='/images/reddit-logo-app.jpg' hieght='sm' />
-          <Heading>Reddit</Heading>
+          <Image src='/images/redditFace.svg' height='30px' />
+          <Image src='/images/redditText.svg' height='46px' />
         </Flex>
     </Flex>
   )
