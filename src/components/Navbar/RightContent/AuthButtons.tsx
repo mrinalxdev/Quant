76e3177888cbfa5d5d73204e45a89e40ec9eaab4
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-25 00:43:14 +0530

    Added Auth Buttons

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -3,11 +3,28 @@ import { Button } from '@chakra-ui/react'
 
 
 
+
 const AuthButtons:React.FC = () => {
   return (
     <>
-        <Button>Log In</Button>
-        <Button>Sign In</Button>
+        <Button 
+          variant='outline' 
+          height='28px' 
+          display={{  base:'none', sm: 'flex'}} 
+          width={{ base: '70px', md:'110px'}} 
+          mr={2} 
+          // onClick={() =>  {}}
+          >
+            Log In
+        </Button>
+        <Button height='28px' 
+          display={{  base:'none', sm: 'flex'}} 
+          width={{ base: '70px', md:'110px'}} 
+          mr={2} 
+          // onClick={() =>  {}}
+          >
+            Sign In
+        </Button>
     </>
   )
 }
