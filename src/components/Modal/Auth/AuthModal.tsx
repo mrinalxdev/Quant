5384a7e9a3668a1be3e143c88fdd297dd147663a
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-25 22:30:08 +0530

    Modal for Auth Buttons

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,43 +1,42 @@
-import React from 'react'
+import React from "react";
 import {
-    useDisclosure, 
-    Button, 
-    Modal, 
-    ModalOverlay, 
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    ModalBody,
-    ModalFooter
-} from '@chakra-ui/react'
-import { useRecoilState } from 'recoil'
-import { authModelState } from '../../../atoms/authModelAtom'
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
+import { authModelState } from "../../../atoms/authModelAtom";
 
+const AuthModal: ReactFC = () => {
+  const [modelState, setModelState] = useRecoilState(authModelState);
 
-const AuthModal:ReactFC = () => {
-    const [modelState, setModelState] = useRecoilState(authModelState)
+  const handleClose = () => {
+    setModelState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
 
-    const handleClose = () => {
-      setModelState((prev) => ({
-        ...prev,
-        open : false,
-      }))
-    }
+  return (
+    <>
+      <Modal isOpen={modelState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <h1> Hello This is Mrinal's Modal</h1>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
-    return (
-      <>
-        <Modal isOpen={modelState.open} onClose={handleClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <h1> Hello This is Mrinal's Modal</h1>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </>
-    )
-}
-
-export default AuthModal
+export default AuthModal;
--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import { Button } from '@chakra-ui/react'
+import React from "react";
+import { Button } from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
+import { authModelState } from '../../../atoms/authModelAtom'
 
+const AuthButtons: React.FC = () => {
+  const setAuthModelState = useSetRecoilState(authModelState);
 
-
-
-const AuthButtons:React.FC = () => {
   return (
     <>
-        <Button 
-          variant='outline' 
-          height='28px' 
-          display={{  base:'none', sm: 'flex'}} 
-          width={{ base: '70px', md:'110px'}} 
-          mr={2} 
-          // onClick={() =>  {}}
-          >
-            Log In
-        </Button>
-        <Button height='28px' 
-          display={{  base:'none', sm: 'flex'}} 
-          width={{ base: '70px', md:'110px'}} 
-          mr={2} 
-          // onClick={() =>  {}}
-          >
-            Sign In
-        </Button>
+      <Button
+        variant="outline"
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        // onClick={() =>  {}}
+      >
+        Log In
+      </Button>
+      <Button
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        // onClick={() =>  {}}
+      >
+        Sign In
+      </Button>
     </>
-  )
-}
+  );
+};
 
-export default AuthButtons
+export default AuthButtons;
