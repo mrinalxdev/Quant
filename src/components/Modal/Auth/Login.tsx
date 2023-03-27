a956911c6789e07e502acad58b369f8d72fbbd1b
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-27 22:58:25 +0530

    Added New Data

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -4,19 +4,36 @@ import { Input, Button } from "@chakra-ui/react";
 type LoginProps = {};
 
 const Login: React.FC<LoginProps> = () => {
-  const [loginFomr, setLoginForm] = useState();
+  const [loginFomr, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (event: React.ChnageEvent<HTMLInputElement>) => {
+    setLoginForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const onSubmit = () => {};
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <Input
         name="email"
         placeholder="Enter your emial"
         mb={2}
-        onChange={() => {}}
+        onChange={onChange}
       />
-      <Input name='password' placeholder='Password' type='password' onChange={() => {}}/>
+      <Input
+        name="password"
+        placeholder="Password"
+        type="password"
+        onChange={onChange}
+      />
 
-      <Button>Log In</Button>
+      <Button width='100%' height='36px' mt={2} mb={2} type="submit">Log In</Button>
     </form>
   );
 };
