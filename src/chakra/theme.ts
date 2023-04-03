8bd22bbd0bd3939fd3dd047974466ed390d6c8ca
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-03 15:15:54 +0530

    Clean Code

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,29 +1,28 @@
-import { extendTheme } from "@chakra-ui/react"
-import { Button } from './button'
-import '@fontsource/open-sans/300.css'
-import '@fontsource/open-sans/400.css'
-import '@fontsource/open-sans/700.css'
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./button";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
 
 export const theme = extendTheme({
   colors: {
     brand: {
       100: "#ff3c00",
-
     },
   },
 
   fonts: {
-    body : 'Open Sans, sans-serif'
+    body: "Open Sans, sans-serif",
   },
 
-  styles : {
-    global : () => ({
-        body : {
-            bg : "gray.200",
-        },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
     }),
   },
-  components : {
+  components: {
     Button,
-  }
-})
+  },
+});
