1c12bad83f40aed3816046d330cb17aeccc6b905
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-03 15:10:45 +0530

    Added New Data

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import { SearchIcon } from '@chakra-ui/icons'
-import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import React from "react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 type SearchInputProps = {
-    // user : 
-}
+  // user :
+};
 
-const SearchInput:React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = () => {
   return (
-    <Flex flexGrow={1} mr={2} align='center'>
-        <InputGroup>
-            <InputLeftElement
-                pointerEvents='none'
-                children={<SearchIcon color='gray.300' mb={1}
-                />}
-             />
-            <Input 
-              type='text' 
-              placeholder='Search Reddit' 
-              fontSize='10pt'
-              _placeholder= {{ color : 'grey.500'}}
-              _hover={{
-                bg: 'white',
-                border: '1px solid',
-                borderColor: 'blue.500',
-              }}
-              _focus={{
-                outline: 'none',
-                border: '1px solid',
-                borderColor: 'blue.500',
-              }}
-              height='34px'
-              bg='grey.50'
-            />
-        </InputGroup>
+    <Flex flexGrow={1} mr={2} align="center">
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" mb={1} />}
+        />
+        <Input
+          type="text"
+          placeholder="Search in  Reddit for content and communities"
+          fontSize="10pt"
+          _placeholder={{ color: "grey.500" }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          height="34px"
+          bg="grey.50"
+        />
+      </InputGroup>
     </Flex>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
