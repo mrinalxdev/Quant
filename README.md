28ee4a2d479894042958b48bfb5e97a1373625af
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-23 01:22:43 +0530

    Added New Data

---
+++ README.md
@@ -0,0 +1,2 @@
+# MindMingle
+A platform where everyone will share their thoughts discuss on there ideas and speak in the public .
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css"
+
+function App() {
+  return (
+    <h1>This is MindMingle</h1>
+  );
+}
+
+export default App;
