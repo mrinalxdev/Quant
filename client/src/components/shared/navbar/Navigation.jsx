1a6be75ca6ac789fa5cf1b72dce773863a00b9c1
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-23 13:29:55 +0530

    Added New Data

--- client/src/components/shared/navbar/Navigation.jsx
+++ client/src/components/shared/navbar/Navigation.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Navigation.module.css'
 
 const Navigation = () => {
-  return (
-    <div>
-      Navigation
-    </div>
-  )
-}
 
-export default Navigation
+  return (
+    <nav className={`${styles.navbar} container`}>
+      <Link to="/" className={styles.logo}>
+        <h1>MindMingle</h1>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navigation;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import styles from './Home.module.css'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className={styles.card}>
+      <div className={styles.headingWrapper}>
+        <span>👋</span>
+        <h1>Welcome to MindMingle !</h1>
+      </div>
+      <p>
+        We are working hard to get MindMingle ready for everyone! While <br /> we wrap up the finishing youches, we are adding people gradually <br /> to make sure nothing breaks
+      </p>
+      <div>
+        <button>Get your Username 👉 </button>
+      </div>
+      <div>
+        <span>Have an invite text ?</span>
+        <Link to="/login">Sign In</Link>
+      </div>
     </div>
   )
 }
