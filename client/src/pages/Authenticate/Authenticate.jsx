634ddbc19d85de363384c6fbf807e59c214a205b
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-24 22:44:59 +0530

    Added New Data

--- client/src/pages/Authenticate/Authenticate.jsx
+++ client/src/pages/Authenticate/Authenticate.jsx
@@ -8,7 +8,7 @@ const steps = {
 };
 
 const Authenticate = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
   const Step = steps[step];
 
   const onClick = () => {
--- client/src/pages/Steps/StepOtp/StepOtp.jsx
+++ client/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./StepOtp.module.css";
+import Card from "../../../components/Card/Card";
+import Input from "../../../components/shared/input/Input";
+import Button from "../../../components/shared/button/Button";
 
 const StepOtp = ({ onClick }) => {
+  const [otp, setOtp] = useState("");
+
+  const next = () => {};
+
   return (
     <div>
-      StepOtp
-
-      <button onClick={onClick}>Next</button>
+      <div className={styles.cardWrapper}>
+        <Card title="Enter the code we just texted you" logoText="🔒">
+          <Input value={otp} onChange={(e) => setOtp(e.target.value)} />
+          <div className={styles.actionButton}>
+            <Button onClick={next} text="Next" />
+          </div>
+          <p className={styles.bottomPara}>
+            Thanks for accepting our Terms of Service and Privacy Policy 
+          </p>
+        </Card>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StepOtp
+export default StepOtp;
