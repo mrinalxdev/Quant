3b0e78cac97357d0dc86eff9adaaf46c021543fe
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-07-03 21:45:13 +0530

    Added New Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Dashboard from './pages/Dashboard/Dashboard'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <>
+    <Dashboard />
     </>
   )
 }
---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Sidenav = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default Sidenav
