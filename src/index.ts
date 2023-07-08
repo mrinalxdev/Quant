18e3b25a7f12287ae6b6b286f11ea254e7675c84
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-07-09 00:57:19 +0530

    Added Main frame logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-import 'dotenv/config'
+import "dotenv/config";
+import "module-alias/register";
+import validateEnv from "@/utils/validateEnv";
+import App from "./app";
+
+validateEnv();
+
+const app = new App([], Number(process.env.PORT));
+app.listen();
