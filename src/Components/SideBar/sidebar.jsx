7244c9ad1ef02202fd238d5ae849e9ff02974691
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-07 22:44:06 +0530

    Added New Data

--- src/Components/SideBar/sidebar.jsx
+++ src/Components/SideBar/sidebar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import {IoMdSpeedometer} from "react-icons/io"
+import {MdDeliveryDining, MdOutlineExplore, MdOutlinePermContactCalendar} from 'react-icons/md'
+import {BsTrophy} from 'react-icons/bs'
+import {AiOutlinePieChart} from 'react-icons/ai'
+import {BiTrendingUp} from 'react-icons/bi'
 import './sidebar.css'
 
 const Sidebar = () => {
@@ -16,7 +21,82 @@ const Sidebar = () => {
 
           <li className="listItem">
             <a href="#" className='menuLink flex'>
+                <IoMdSpeedometer className/>
+                <span className="smallText">
+                  Dash Board
+                </span>
+            </a>
+          </li>
 
+          <li className="listItem">
+            <a href="#" className='menuLink flex'>
+                <MdDeliveryDining className/>
+                <span className="smallText">
+                  My Order
+                </span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className='menuLink flex'>
+                <MdOutlineExplore className/>
+                <span className="smallText">
+                 Explore
+                </span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className='menuLink flex'>
+                <BsTrophy className/>
+                <span className="smallText">
+                  Products
+                </span>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+
+      <div className="settingsDiv">
+        <h3 className="divTitle">
+         SETTINGS
+        </h3>
+        <ul className="menuLists grid">
+
+          <li className="listItem">
+            <a href="#" className='menuLink flex'>
+                <AiOutlinePieChart className/>
+                <span className="smallText">
+                  Dash Board
+                </span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className='menuLink flex'>
+                <BiTrendingUp className/>
+                <span className="smallText">
+                  Trends
+                </span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className='menuLink flex'>
+                <MdOutlineExplore className/>
+                <span className="smallText">
+                 Explore
+                </span>
+            </a>
+          </li>
+
+          <li className="listItem">
+            <a href="#" className='menuLink flex'>
+                <MdOutlinePermContactCalendar className/>
+                <span className="smallText">
+                  Contacts
+                </span>
             </a>
           </li>
 
