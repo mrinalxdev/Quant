6140d5188a9e391933b37ba721f70e20388cd8d0
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-11 21:49:50 +0530

    Added New Data

--- app/components/FeedbackItem.js
+++ app/components/FeedbackItem.js
@@ -1,23 +1,27 @@
 import React from "react";
 
-const FeedbackItem = () => {
+const FeedbackItem = ({ onOpen, title, description, votesCount }) => {
   return (
-    <div className="my-8 flex items-center gap-6">
+    <a
+      href=""
+      onClick={(e) => {
+        e.preventDefault();
+        onOpen();
+      }}
+      className="my-8 flex items-center gap-6"
+    >
       <div>
-        <h2 className="font-bold">Nice thought Mrinal !</h2>
-        <p className="text-gray-600 text-sm ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          nesciunt voluptatibus debitis ea corporis exercitationem voluptatum
-          nemo excepturi totam error.
-        </p>
+        <h2 className="font-bold">{title}</h2>
+        <p className="text-gray-600 text-sm ">{description}</p>
       </div>
       <div>
         <button className="shadow-sm shadow-gray-400 border rounded-md px-3 py-2 flex items-center gap-4 text-gray-600">
           {" "}
-          <span className="triangle"></span>20
+          <span className="triangle"></span>
+          {votesCount}
         </button>
       </div>
-    </div>
+    </a>
   );
 };
 
