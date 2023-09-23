4ef1f3209728eeaab6a79281e83f6e465cf9115e
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-09-23 12:26:34 +0530

    New vps

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const cors = require("require")
-const mongoose = require("mongoose")
-
-require("dotenv").config()
-const app = express()
-
-const PORT = process.env.PORT || 5000
